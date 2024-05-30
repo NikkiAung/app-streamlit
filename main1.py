c1ad5f8be425af8ba5124549ba63abec265ae073
Aung Nanda Oo <160472666+NikkiAung@users.noreply.github.com>
2024-05-30 00:20:25 -0700

    Update main1.py

--- main1.py
+++ main1.py
@@ -3,13 +3,14 @@ from langchain_helper import get_qa_chain, create_vector_db
 
 st.title("Codebasics Q&A 🌱")
 btn = st.button("Create Knowledgebase")
-
 if btn:
     create_vector_db()
-    st.success("Knowledgebase created successfully!")
 
-question = st.text_input("Enter your question:")
+question = st.text_input("Question: ")
+
 if question:
     chain = get_qa_chain()
-    answer = chain.invoke({"query": question})
-    st.write("Answer:", answer)
+    response = chain(question)
+
+    st.header("Answer")
+    st.write(response["result"])
